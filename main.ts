10859bed2f237e40b7d8c22513b3ebd11614991d
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-19 20:49:10 -0400

    Temprature

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
+ * This program will show our current temperature.
 */
+let temprature = input.temperature()
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+
+
+
+input.onButtonPressed(Button.A, function () {
+    temprature = input.temperature()
+   
+    
+    basic.showString('Temprature is:')
+    basic.showNumber(temprature)
+
+    
+    })
+   
